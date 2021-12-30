309d50fcc7b8ffa43629129184d4d3505b1a81f8
Robert Isoski <r@robiso.com>
2021-12-30 23:51:10 +0100

    Update theme.php

--- theme.php
+++ theme.php
@@ -23,10 +23,9 @@
 		<title><?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?></title>
 
 		<link rel="stylesheet" rel="preload" as="style" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-		<link rel="stylesheet" rel="preload" as="style" href="<?= $Wcms->asset('css/style.css') ?>">
-
 		<?= $Wcms->css() ?>
 
+		<link rel="stylesheet" rel="preload" as="style" href="<?= $Wcms->asset('css/style.css') ?>">
 	</head>
 
 	<body class="d-flex flex-column">
